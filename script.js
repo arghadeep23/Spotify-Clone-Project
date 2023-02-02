a7de66eb580cd8dc714bedd1e26e5130f794642e
Arghadeep Dey <91934528+arghadeep23@users.noreply.github.com>
2023-02-02 21:06:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Welcome to Spotify");
+let songs=[
+    {songName:"Calm Down", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Calm Down", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Calm Down", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Calm Down", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Calm Down", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Calm Down", filePath:"song/1.mp3",coverPath:"covers/1.jpg"}
+]
+let audioElement = new Audio('motivational-electronic-distant-132919.mp3');
+audioElement.play();
+// Listen to events 
